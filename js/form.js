bee5a990a8af1810aacc7c00e35b16904e265618
falkhorus <falkhorus@gmail.com>
2025-10-19 16:44:59 -0300

    Atualização 2.0

--- js/form.js
+++ js/form.js
@@ -1,27 +1,41 @@
 
-//class contato
+let contatoArr = [];
 
 class contato {
-    
-}
-
-function Post(form) {
-
-  let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
-            form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
-            form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
-  
-}
-
-function Enviar() {
-
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    constructor(nome, sobrenome, email, cpf, telefone, contato) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.contato = contato;
     }
-
 }
+
+window.addEventListener('DOMContentLoaded', (event) => { // Garantir que o DOM esteja carregado antes de adicionar o listener
+    const form = document.getElementById("formid");
+
+    form.addEventListener("submit", function(event) { // Adicionar listener de evento de submit
+        event.preventDefault(); // Para prevenir o comportamento padrão do formulário de recarregar a página
+        
+        let data = new contato(
+            form.elements.namedItem("nome").value,
+            form.elements.namedItem("sobrenome").value,
+            form.elements.namedItem("email").value,
+            form.elements.namedItem("cpf").value,
+            form.elements.namedItem("telefone").value,
+            form.elements.namedItem("contato").value
+        );
+        
+        contatoArr.push(data); // Adiciona o novo contato ao final do array
+        
+        console.log("Array de contatos foi atualizado:");
+        console.log(contatoArr);
+
+        if (data.nome !== "") {  // Verifica se o nome contido no objeto (contato) não está vazio, para poder exibir a mensagem de alerta
+            alert('Obrigado sr(a) ' + data.nome + ' os seus dados foram encaminhados com sucesso');
+        }
+
+        form.reset(); // Limpa os campos do formulário após o envio
+    });
+});
